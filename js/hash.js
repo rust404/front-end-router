04428520b5f43f55443ea1a9deee365131595af9
rust404 <rust404@outlook.com>
2020-11-24 10:18:32 +0800

    使用类来初始化路由

--- js/hash.js
+++ js/hash.js
@@ -1,23 +1,44 @@
-let routerView
+class HashRouter {
+  constructor({routes, routerView}) {
+    this.routes = routes
+    this.routerView = routerView
 
-const routes = {
-  '/a': '<div>hello, a</div>',
-  '/b': '<div>hello, b</div>'
-}
-
-const onLoad = () => {
-  routerView = document.querySelector('#router-view')
-  onHashChange()
-}
-
-const onHashChange = () => {
-  const path = window.location.hash.slice(1)
-  if (routes.hasOwnProperty(path)) {
-    routerView.innerHTML = routes[path]
-  } else {
-    routerView.innerHTML = ''
+    window.addEventListener('hashchange', this.onHashChange)
+    window.addEventListener('DOMContentLoaded', this.onLoad)
+  }
+  push(path) {
+    window.location.hash = path
+  }
+  back() {
+    window.history.back()
+  }
+  onHashChange = () => {
+    const path = window.location.hash.slice(1)
+    const route = this.matchRoute(path)
+    if (route) {
+      this.routerView.innerHTML = route.component
+    } else {
+      this.routerView.innerHTML = ''
+    }
+  }
+  onLoad = () => {
+    this.onHashChange()
+  }
+  matchRoute(path) {
+    return this.routes.filter(route => route.path === path)[0]
   }
 }
 
-window.addEventListener('hashchange', onHashChange)
-window.addEventListener('DOMContentLoaded', onLoad)
+new HashRouter({
+  routes:[
+    {
+      path: '/a',
+      component: '<div>hello, a</div>'
+    },
+    {
+      path: '/b',
+      component: '<div>hello, b</div>'
+    }
+  ],
+  routerView: document.querySelector('#router-view')
+})
--- js/history.js
+++ js/history.js
@@ -1,32 +1,52 @@
-let routerView
+class HistoryRouter {
+  constructor({routes, routerView}) {
+    this.routes = routes
+    this.routerView = routerView
 
-const routes = {
-  '/a': '<div>hello, a</div>',
-  '/b': '<div>hello, b</div>'
-}
-
-const onLoad = () => {
-  routerView = document.querySelector('#router-view')
-  const links = document.querySelectorAll('a[href]')
-  links.forEach(link => {
-    link.addEventListener('click', function (e) {
-      e.preventDefault()
-      const href = link.getAttribute('href')
-      window.history.pushState(null, '', href)
-      onPopState()
+    window.addEventListener('popstate', this.onPopState)
+    window.addEventListener('DOMContentLoaded', this.onLoad)
+  }
+  push(path) {
+    window.history.pushState(null, null, path)
+  }
+  back() {
+    window.history.back()
+  }
+  onPopState = () => {
+    const path = window.location.pathname
+    const route = this.matchRoute(path)
+    if (route) {
+      this.routerView.innerHTML = route.component
+    } else {
+      this.routerView.innerHTML = ''
+    }
+  }
+  onLoad = () => {
+    const links = document.querySelectorAll('a[href].router-link')
+    links.forEach(link => {
+      link.addEventListener('click',  (e) => {
+        e.preventDefault()
+        const href = link.getAttribute('href')
+        window.history.pushState(null, '', href)
+        this.onPopState()
+      })
     })
-  })
-  onPopState()
-}
-
-const onPopState = () => {
-  const path = window.location.pathname
-  if (routes.hasOwnProperty(path)) {
-    routerView.innerHTML = routes[path]
-  } else {
-    routerView.innerHTML = ''
+    this.onPopState()
+  }
+  matchRoute(path) {
+    return this.routes.filter(route => route.path === path)[0]
   }
 }
-
-window.addEventListener('popstate', onPopState)
-window.addEventListener('DOMContentLoaded', onLoad)
+new HistoryRouter({
+  routes:[
+    {
+      path: '/a',
+      component: '<div>hello, a</div>'
+    },
+    {
+      path: '/b',
+      component: '<div>hello, b</div>'
+    }
+  ],
+  routerView: document.querySelector('#router-view')
+})
